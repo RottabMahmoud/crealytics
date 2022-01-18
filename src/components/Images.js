ffa1c34af01146b626d5eb7a6c649c58c6ba55f3
Mahmoud Rottab <rottabmahmoud49@gmail.com>
2022-01-18 02:00:00 +0200

    Eh Yabne dah: hahahahaha el sa3a 10 we nos ,,...,....,,.,.....,. .,.,.,.,.,.,. .,.,.,,,,,...,

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import Image from "react-async-image";
+// import Image from "react-async-image";
 
 export default function Images({ itemData }) {
   /** The Array of Image to be rendered in display once item is expanded, filled with objects,
@@ -19,7 +19,7 @@ export default function Images({ itemData }) {
     >
       {imagesArray.map((item) => (
         <ImageListItem key={item.img}>
-          <Image
+          <img
             src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
             srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
             alt={item.img}
